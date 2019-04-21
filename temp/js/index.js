f5dfca622416afb72074673a9284893414184fb9
Troy <Troywu9527@gmail.com>
2019-04-20 21:48:22 -0400

    typing text effect and sidenav taps

--- temp/js/index.js
+++ temp/js/index.js
@@ -1,1 +1,24 @@
 console.log("hello world");
+
+var i=0, text;
+text = "Welcome To Troy Web";
+
+function typingText() {
+    if (i<text.length) {
+        document.getElementById('text').innerHTML += text.charAt(i);
+        i++;
+        // duration of effecyt
+        setTimeout(typingText, 50);
+    }
+    document.getElementById('text').innerHTML = "";
+};
+
+function clearText() {
+    document.getElementById('text').innerHTML = "";
+}
+
+setInterval(typingText(), 3000);
+
+// window.setInterval(function () {
+//     typingText();
+// }, 100);
